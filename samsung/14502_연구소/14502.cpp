82d84c33055d51a4196ceddaad977bdc0a4c794b
jinwookss <jjw6215@gmail.com>
2020-05-03 00:59:52 +0900

    14502번 풀이 추가

--- samsung/14502_연구소/14502.cpp
+++ samsung/14502_연구소/14502.cpp
@@ -5,8 +5,11 @@
  */
 #include <fstream>
 #include <iostream>
+#include <queue>
 
 using namespace std;
+
+#define Point pair<int, int>
 
 enum {
     EMPTY = 0,
@@ -15,9 +18,11 @@ enum {
 };
 
 int N, M;  // 세로, 가로 (3 <= N, M <= 8)
+
 int map[8][8] = {
     0,
 };
+
 int wall[3] = {
     0,
 };
@@ -25,10 +30,12 @@ int wall[3] = {
 int dy[4] = {-1, 1, 0, 0};
 int dx[4] = {0, 0, -1, 1};
 
+int maxSafeZone = 0;
+
 int getNext(int pos) {
     while (pos < N * M) {
-        int y = pos / N;
-        int x = pos % N;
+        int y = pos / M;
+        int x = pos % M;
 
         if (map[y][x] == EMPTY) {
             return pos;
@@ -47,24 +54,74 @@ bool makeWall() {
     } else {
         // 기존 벽 삭제
         for (int i = 0; i < 3; i++) {
-            int y = wall[i] / N;
-            int x = wall[i] % N;
+            int y = wall[i] / M;
+            int x = wall[i] % M;
             map[y][x] = EMPTY;
         }
 
-        if ((wall[2] = getNext(wall[2] + 1)) == -1) {
-            if ((wall[1] = getNext(wall[1] + 1)) == -1) {
-                if ((wall[0] = getNext(wall[0] + 1)) == -1) {
-                    return false;
+        while (true) {
+            if (wall[2] == -1 || (wall[2] = getNext(wall[2] + 1)) == -1) {
+                if (wall[1] == -1 || (wall[1] = getNext(wall[1] + 1)) == -1) {
+                    if ((wall[0] = getNext(wall[0] + 1)) == -1) {
+                        return false;
+                    }
+                    wall[1] = getNext(wall[0] + 1);
                 }
-                wall[1] = getNext(wall[0] + 1);
                 wall[2] = getNext(wall[1] + 1);
-            }
-            wall[2] = getNext(wall[1] + 1);
+            } 
+
+            if (wall[0] != -1 && wall[1] != -1 && wall[2] != -1) break;
         }
     }
-    cout << wall[0] << ", " << wall[1] << ", " << wall[2] << endl;
-    return true;
+
+    return wall[0] < wall[1] && wall[1] < wall[2];
+}
+
+void spreadVirus() {
+    
+    int tempMap[8][8] = {0, };
+    queue<Point> q;
+    bool visited[8][8] = {false, };
+
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < M; x++) {
+            tempMap[y][x] = map[y][x];
+            if (tempMap[y][x] == VIRUS) {
+                q.push(Point(y, x));
+                visited[y][x] = true;
+                // cout << "virus(" << y << ", " << x << ")" << endl;
+            }
+        }
+    }
+
+    while (!q.empty()) {
+        Point p = q.front(); q.pop();
+
+        for (int dir = 0; dir < 4; dir++) {
+            int ny = p.first + dy[dir];
+            int nx = p.second + dx[dir];
+            
+            if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+
+            if (tempMap[ny][nx] == EMPTY && !visited[ny][nx]) {
+                visited[ny][nx] = true;
+                tempMap[ny][nx] = VIRUS;
+                q.push(Point(ny, nx));
+            }
+        }
+    }
+
+    int safeZone = 0;
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < M; x++) {
+            if (tempMap[y][x] == EMPTY) {
+                safeZone++;
+            }
+        }
+    }
+    if (safeZone > maxSafeZone) {
+        maxSafeZone = safeZone;
+    }
 }
 
 int main() {
@@ -78,13 +135,20 @@ int main() {
         }
     }
 
+    // 벽을 세울 수 있는 모든 경우의 수를 체크하면서 
     while (makeWall()) {
+        // cout << wall[0] << ", " << wall[1] << ", " << wall[2] << endl;
+        // 지도에 벽을 그리고
         for (int i = 0; i < 3; i++) {
-            int y = wall[i] / N;
-            int x = wall[i] % N;
+            int y = wall[i] / M;
+            int x = wall[i] % M;
             map[y][x] = WALL;
         }
+        // 바이러스를 확산시켜보며 최대 안전 영역 계산
+        spreadVirus();
     }
+
+    cout << maxSafeZone << endl;
 
     return 0;
 }
