8bae2740f6e1cb301fc138bcfeb3b0205822a72c
jinwookss <jjw6215@gmail.com>
2020-07-06 00:51:02 +0900

    디렉토리 정리

---
+++ Base/DataStructure/Tree/Heap/heap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int HEAP_SIZE = 200;
+int heap[HEAP_SIZE] = {1, 2, 3, 1, 4, 5, 2, 3, 6};
+
+void heapify(int i) {
+    int cur = i * 2;
+
+    if (cur < n )
+}
+
+int main() {
+
+
+    return 0;
+}
---
+++ Base/Search/README.md
@@ -0,0 +1,3 @@
+# Search Algotithms
+
+https://namu.wiki/w/%EB%B6%84%EB%A5%98:%ED%83%90%EC%83%89%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98
