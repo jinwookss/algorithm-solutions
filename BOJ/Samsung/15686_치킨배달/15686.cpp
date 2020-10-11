618ec60d99824edcfb7d716bbe3d985099176adf
jinwookss <jjw6215@gmail.com>
2020-10-11 18:13:10 +0900

    치킨 배달 풀이 추가

---
+++ BOJ/Samsung/15686_치킨배달/15686.cpp
@@ -0,0 +1,70 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAX_SIZE = 51; // [2, 50]
+enum {
+    EMPTY = 0,
+    HOME = 1,
+    CHICKEN = 2,  
+};
+
+int N, M;
+int map[MAX_SIZE][MAX_SIZE];
+vector<int> chickens;
+vector<int> homes;
+vector<int> selectedChickens;
+int minCityDistance = INT32_MAX;
+
+int getDistance(int a, int b) {
+    return abs((a / (N + 1)) - (b / (N + 1))) + abs((a % (N + 1)) - (b % (N + 1)));
+}
+
+int getCityChickenDistance() {
+    int cityDistance = 0;
+    for (int i = 0; i < homes.size(); i++) {
+        int distance = INT32_MAX;
+        for (int j = 0; j < selectedChickens.size(); j++) {
+            distance = min(distance, getDistance(homes[i], selectedChickens[j]));
+        }
+        cityDistance += distance;
+    }
+    return cityDistance;
+}
+
+void selectChickens(int count, int max, int idx) {
+    if (count == max) {
+        minCityDistance = min(minCityDistance, getCityChickenDistance());
+        return;
+    }
+
+    for (int i = idx; i < chickens.size(); i++) {
+        selectedChickens.push_back(chickens[i]);
+        selectChickens(count + 1, max, i + 1);
+        selectedChickens.pop_back();
+    }
+}
+
+int main() {
+    ifstream cin("input.txt");
+    if (cin.fail()) return -1;
+
+    cin >> N >> M;
+    for (int y = 1; y <= N; y++) {
+        for (int x = 1; x <= N; x++) {
+            cin >> map[y][x];
+            if (map[y][x] == CHICKEN) {
+                chickens.push_back(y * (N + 1) + x);
+            } else if (map[y][x] == HOME) {
+                homes.push_back(y * (N + 1) + x);
+            }
+        }
+    }
+    
+    selectChickens(0, M, 0);
+    cout << minCityDistance << endl;
+
+    return 0;
+}
